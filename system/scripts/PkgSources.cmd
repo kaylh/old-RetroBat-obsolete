2211a6b3253c1ce954461596af48250a17735db6
kaylh <adriencd@hotmail.fr>
2019-12-19 15:01:24 +0100

    Update PkgSources.cmd

--- system/scripts/PkgSources.cmd
+++ system/scripts/PkgSources.cmd
@@ -6,7 +6,7 @@ This file is part of RetroBat Scripts.
 :rem
 set emulationstation_url="https://github.com/fabricecaruso/emulationstation/releases/download/continuous-master/emulationstation-win32.zip"
 set emulationstation_update_url="https://github.com/fabricecaruso/emulationstation/releases/download/continuous-master/emulationstation-win32-no-deps.zip"
-set default_theme_url="https://github.com/kaylh/es-theme-nextfull/archive/master.zip"
+set default_theme_url="https://github.com/kaylh/es-theme-%themename%/archive/master.zip"
 set retroarch_stable_url="https://buildbot.libretro.com/stable/1.8.1/windows/x86_64/RetroArch.7z"
 set retroarch_stable_update_url="https://buildbot.libretro.com/stable/1.8.1/windows/x86_64/RetroArch_update.zip"
 set retroarch_nightly_url="https://buildbot.libretro.com/nightly/windows/x86_64/retroarch.7z"
