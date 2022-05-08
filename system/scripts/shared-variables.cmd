01d1ddd0ea523af9ba505b9f4079e58d0066ce7b
Kayl <adriencd@hotmail.fr>
2022-05-08 15:24:20 +0200

    Update shared-variables.cmd

--- system/scripts/shared-variables.cmd
+++ system/scripts/shared-variables.cmd
@@ -23,6 +23,23 @@ if "%archx%"=="x86_64" (
 echo archx=%archx%>> "%tmp_infos_file%"
 echo arch=%arch%>> "%tmp_infos_file%"
 
+:: ---- SET INFOS ABOUT RETROBAT VERSION ----
+
+set version_file_local=version.info
+set version_file_remote=remote_version.info
+	
+cd "%root_path%\emulationstation"
+set/p version_local=<%version_file_local%
+(echo version_local=%version_local%)>> "%tmp_infos_file%"
+
+if exist "%root_path%\system\modules\rb_updater\%version_file_remote%" del/Q "%root_path%\system\modules\rb_updater\%version_file_remote%" >nul
+
+if not exist "%root_path%\system\modules\rb_updater\%version_file_remote%" "%root_path%\system\modules\rb_updater\wget" --no-check-certificate wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 3 -P "%root_path%\system\modules\rb_updater" https://www.retrobat.ovh/repo/%arch%/%branch%/%version_local%/%version_file_remote% -q >nul
+
+cd "%root_path%\system\modules\rb_updater"
+set/p version_remote=<%version_file_remote%
+(echo version_remote=%version_remote%)>> "%tmp_infos_file%"
+
 :: ---- RETROBAT PATHS ----
 
 echo batgui_path=!root_path!>> "%tmp_infos_file%"
@@ -35,40 +52,26 @@ echo emulationstation_path=!root_path!\emulationstation>> "%tmp_infos_file%"
 echo emulators_path=!root_path!\emulators>> "%tmp_infos_file%"
 echo extraction_path=!root_path!\system\download\extract>> "%tmp_infos_file%"
 echo lrcores_path=!root_path!\emulators\retroarch\cores>> "%tmp_infos_file%"
+echo mega_bezels_path=!root_path!\emulators\retroarch\shaders\shaders_slang>> "%tmp_infos_file%"
 echo retroarch_path=!root_path!\emulators\retroarch>> "%tmp_infos_file%"
+echo retrobat_path=!root_path!>> "%tmp_infos_file%"
 echo roms_path=!root_path!\roms>> "%tmp_infos_file%"
 echo saves_path=!root_path!\saves>> "%tmp_infos_file%"
 echo shaders_path=!root_path!\system\shaders>> "%tmp_infos_file%"
 echo system_path=!root_path!\system>> "%tmp_infos_file%"
 
-:: ---- SET INFOS ABOUT RETROBAT VERSION ----
-
-set version_file_local=version.info
-set version_file_remote=remote_version.info
-
-if exist "!root_path!\system\modules\rb_updater\%version_file_remote%" del/Q "!root_path!\system\modules\rb_updater\%version_file_remote%" 
-
-if not exist "!root_path!\system\modules\rb_updater\%version_file_remote%" "!root_path!\system\modules\rb_updater\wget" --no-check-certificate wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 3 -P "!root_path!\system\modules\rb_updater" https://www.retrobat.ovh/repo/%arch%/%branch%/%version_file_remote% -q >nul
-
-cd !root_path!\system\modules\rb_updater
-set/p version_remote=<%version_file_remote%
-echo version_remote=%version_remote%>> "%tmp_infos_file%"
-
-cd !root_path!\emulationstation
-set/p version_local=<%version_file_local%
-echo version_local=%version_local%>> "%tmp_infos_file%"
-
 :: ---- URLS ----
 
 echo batgui_url=https://www.retrobat.ovh/repo/%arch%/%branch%>> "%tmp_infos_file%"
-echo batocera_ports_url=https://github.com/fabricecaruso/batocera-ports/releases/tag/continuous>> "%tmp_infos_file%"
-echo bios_url=https://www.retrobat.ovh/repo/%arch%/%branch%>> "%tmp_infos_file%"
-echo decorations_url=https://www.retrobat.ovh/repo/%arch%/%branch%>> "%tmp_infos_file%"
+echo batocera_ports_url=https://github.com/fabricecaruso/batocera-ports/releases/download/continuous>> "%tmp_infos_file%"
+echo bios_url=https://github.com/kaylh/RetroBat-BIOS/archive/refs/heads>> "%tmp_infos_file%"
+echo decorations_url=https://www.retrobat.ovh/repo/decorations>> "%tmp_infos_file%"
 echo default_theme_url=https://github.com/fabricecaruso/es-theme-carbon/archive/refs/heads>> "%tmp_infos_file%"
-echo emulationstation_url=https://github.com/fabricecaruso/batocera-emulationstation/releases/tag/continuous-master>> "%tmp_infos_file%"
+echo emulationstation_url=https://github.com/fabricecaruso/batocera-emulationstation/releases/download/continuous-master>> "%tmp_infos_file%"
 echo lrcores_url=https://buildbot.libretro.com/nightly/windows/%archx%/latest>> "%tmp_infos_file%"
+echo mega_bezels_url=https://www.retrobat.ovh/repo/decorations>> "%tmp_infos_file%"
 echo releases_url=https://www.retrobat.ovh/releases>> "%tmp_infos_file%"
-echo retroarch_url=https://buildbot.libretro.com/%branch%/%retroarch_version%/windows/%archx%>> "%tmp_infos_file%"
+echo retroarch_url=https://buildbot.libretro.com/stable/%retroarch_version%/windows/%archx%>> "%tmp_infos_file%"
 echo retrobat_url=https://www.retrobat.ovh/repo/%arch%/%branch%>> "%tmp_infos_file%"
 
 :: ---- GO BACK TO PARENT SCRIPT ----
