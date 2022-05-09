a86b4ae5e71308b3b0ce18459d5c2c1ac11f185a
Kayl <adriencd@hotmail.fr>
2022-05-09 13:02:37 +0200

    Update shared-variables.cmd

--- system/scripts/shared-variables.cmd
+++ system/scripts/shared-variables.cmd
@@ -9,7 +9,7 @@ It's not intended to run on its own but must be called by other scripts in order
 
 :: ---- GENERAL INFOS ----
 set name=RetroBat
-echo name=%name%>> "%tmp_infos_file%"
+(echo name=%name%)>> "%tmp_infos_file%"
 
 :: ---- PROCESSOR ARCHITECTURE INFO ----
 
@@ -20,8 +20,8 @@ if "%archx%"=="x86_64" (
 ) else (
 	set arch=win32
 )
-echo archx=%archx%>> "%tmp_infos_file%"
-echo arch=%arch%>> "%tmp_infos_file%"
+(echo archx=%archx%)>> "%tmp_infos_file%"
+(echo arch=%arch%)>> "%tmp_infos_file%"
 
 :: ---- SET INFOS ABOUT RETROBAT VERSION ----
 
@@ -42,37 +42,37 @@ set/p version_remote=<%version_file_remote%
 
 :: ---- RETROBAT PATHS ----
 
-echo batgui_path=!root_path!>> "%tmp_infos_file%"
-echo batocera_ports_path=!root_path!\emulationstation>> "%tmp_infos_file%"
-echo bios_path=!root_path!\bios>> "%tmp_infos_file%"
-echo decorations_path=!root_path!\system\decorations>> "%tmp_infos_file%"
-echo default_theme_path=!root_path!\emulationstation\.emulationstation\themes>> "%tmp_infos_file%"
-echo download_path=!root_path!\system\download>> "%tmp_infos_file%"
-echo emulationstation_path=!root_path!\emulationstation>> "%tmp_infos_file%"
-echo emulators_path=!root_path!\emulators>> "%tmp_infos_file%"
-echo extraction_path=!root_path!\system\download\extract>> "%tmp_infos_file%"
-echo lrcores_path=!root_path!\emulators\retroarch\cores>> "%tmp_infos_file%"
-echo mega_bezels_path=!root_path!\emulators\retroarch\shaders\shaders_slang>> "%tmp_infos_file%"
-echo retroarch_path=!root_path!\emulators\retroarch>> "%tmp_infos_file%"
-echo retrobat_path=!root_path!>> "%tmp_infos_file%"
-echo roms_path=!root_path!\roms>> "%tmp_infos_file%"
-echo saves_path=!root_path!\saves>> "%tmp_infos_file%"
-echo shaders_path=!root_path!\system\shaders>> "%tmp_infos_file%"
-echo system_path=!root_path!\system>> "%tmp_infos_file%"
+(echo batgui_path=!root_path!)>> "%tmp_infos_file%"
+(echo batocera_ports_path=!root_path!\emulationstation)>> "%tmp_infos_file%"
+(echo bios_path=!root_path!\bios)>> "%tmp_infos_file%"
+(echo decorations_path=!root_path!\system\decorations)>> "%tmp_infos_file%"
+(echo default_theme_path=!root_path!\emulationstation\.emulationstation\themes)>> "%tmp_infos_file%"
+(echo download_path=!root_path!\system\download)>> "%tmp_infos_file%"
+(echo emulationstation_path=!root_path!\emulationstation)>> "%tmp_infos_file%"
+(echo emulators_path=!root_path!\emulators)>> "%tmp_infos_file%"
+(echo extraction_path=!root_path!\system\download\extract)>> "%tmp_infos_file%"
+(echo lrcores_path=!root_path!\emulators\retroarch\cores)>> "%tmp_infos_file%"
+(echo mega_bezels_path=!root_path!\emulators\retroarch\shaders\shaders_slang)>> "%tmp_infos_file%"
+(echo retroarch_path=!root_path!\emulators\retroarch)>> "%tmp_infos_file%"
+(echo retrobat_path=!root_path!)>> "%tmp_infos_file%"
+(echo roms_path=!root_path!\roms)>> "%tmp_infos_file%"
+(echo saves_path=!root_path!\saves)>> "%tmp_infos_file%"
+(echo shaders_path=!root_path!\system\shaders)>> "%tmp_infos_file%"
+(echo system_path=!root_path!\system)>> "%tmp_infos_file%"
 
 :: ---- URLS ----
 
-echo batgui_url=https://www.retrobat.ovh/repo/%arch%/%branch%>> "%tmp_infos_file%"
-echo batocera_ports_url=https://github.com/fabricecaruso/batocera-ports/releases/download/continuous>> "%tmp_infos_file%"
-echo bios_url=https://github.com/kaylh/RetroBat-BIOS/archive/refs/heads>> "%tmp_infos_file%"
-echo decorations_url=https://www.retrobat.ovh/repo/decorations>> "%tmp_infos_file%"
-echo default_theme_url=https://github.com/fabricecaruso/es-theme-carbon/archive/refs/heads>> "%tmp_infos_file%"
-echo emulationstation_url=https://github.com/fabricecaruso/batocera-emulationstation/releases/download/continuous-master>> "%tmp_infos_file%"
-echo lrcores_url=https://buildbot.libretro.com/nightly/windows/%archx%/latest>> "%tmp_infos_file%"
-echo mega_bezels_url=https://www.retrobat.ovh/repo/decorations>> "%tmp_infos_file%"
-echo releases_url=https://www.retrobat.ovh/releases>> "%tmp_infos_file%"
-echo retroarch_url=https://buildbot.libretro.com/stable/%retroarch_version%/windows/%archx%>> "%tmp_infos_file%"
-echo retrobat_url=https://www.retrobat.ovh/repo/%arch%/%branch%>> "%tmp_infos_file%"
+(echo batgui_url=https://www.retrobat.ovh/repo/%arch%/%branch%)>> "%tmp_infos_file%"
+(echo batocera_ports_url=https://github.com/fabricecaruso/batocera-ports/releases/download/continuous)>> "%tmp_infos_file%"
+(echo bios_url=https://github.com/kaylh/RetroBat-BIOS/archive/refs/heads)>> "%tmp_infos_file%"
+(echo decorations_url=https://www.retrobat.ovh/repo/decorations)>> "%tmp_infos_file%"
+(echo default_theme_url=https://github.com/fabricecaruso/es-theme-carbon/archive/refs/heads)>> "%tmp_infos_file%"
+(echo emulationstation_url=https://github.com/fabricecaruso/batocera-emulationstation/releases/download/continuous-master)>> "%tmp_infos_file%"
+(echo lrcores_url=https://buildbot.libretro.com/nightly/windows/%archx%/latest)>> "%tmp_infos_file%"
+(echo mega_bezels_url=https://www.retrobat.ovh/repo/decorations)>> "%tmp_infos_file%"
+(echo releases_url=https://www.retrobat.ovh/releases)>> "%tmp_infos_file%"
+(echo retroarch_url=https://buildbot.libretro.com/stable/%retroarch_version%/windows/%archx%)>> "%tmp_infos_file%"
+(echo retrobat_url=https://www.retrobat.ovh/repo/%arch%/%branch%)>> "%tmp_infos_file%"
 
 :: ---- GO BACK TO PARENT SCRIPT ----
 
