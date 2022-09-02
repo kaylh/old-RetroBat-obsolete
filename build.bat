19f4424609322bb8062254a44b0a34082d6cb157
Kayl <adriencd@hotmail.fr>
2022-09-02 16:56:34 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -34,7 +34,7 @@ set get_wiimotegun=1
 set deps_list=(git makensis 7za strip wget)
 set submodules_list=(bios default_theme decorations system)
 set packages_list=(retrobat_binaries batgui emulationstation batocera_ports mega_bezels retroarch roms wiimotegun)
-set legacy_cores_list=(4do mame2016 px68k)
+set legacy_cores_list=(4do imageviewer mame2016 px68k)
 set emulators_black_list=(pico8 retroarch ryujinx steam teknoparrot xenia)
 
 :: ---- GET STARTED ----
