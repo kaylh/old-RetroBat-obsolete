c9d8b29ca5bd7d07a9b5ce41fdbb122f51d4232f
Kayl <adriencd@hotmail.fr>
2022-10-29 15:26:41 +0200

    Update shared-variables.cmd

--- system/scripts/shared-variables.cmd
+++ system/scripts/shared-variables.cmd
@@ -15,41 +15,10 @@ set name=retrobat
 
 Reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > nul && set archx=x86 || set archx=x86_64
 
-if "%archx%"=="x86_64" (
-	set arch=win64
-) else (
-	set arch=win32
-)
+if "%archx%"=="x86_64" (set arch=win64)
+if "%archx%"=="x86" (set arch=win32)
 (echo archx=%archx%)>> "%tmp_infos_file%"
 (echo arch=%arch%)>> "%tmp_infos_file%"
-
-:: ---- SET INFOS ABOUT RETROBAT VERSION ----
-
-set version_file_local=version.info
-set version_file_remote=remote_version.info
-
-if not "%script_type%"=="builder" (	
-	cd "%root_path%\emulationstation"
-	set/p version_local=<%version_file_local%
-	(echo version_local=%version_local%)>> "%tmp_infos_file%"
-
-	if exist "%root_path%\system\modules\rb_updater\%version_file_remote%" del/Q "%root_path%\system\modules\rb_updater\%version_file_remote%" >nul
-
-	if not exist "%root_path%\system\modules\rb_updater\%version_file_remote%" "%root_path%\system\modules\rb_updater\wget" --no-check-certificate wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 3 -P "%root_path%\system\modules\rb_updater" https://www.retrobat.ovh/repo/%arch%/%branch%/%version_local%/%version_file_remote% -q >nul
-
-	if %ERRORLEVEL% NEQ 0 (
-
-		set exit_code=%ERRORLEVEL%
-	
-		if exist "%download_path%\*.*" del/Q "%download_path%\*.*"
-		
-		exit !exit_code!	
-	)
-
-	cd "%root_path%\system\modules\rb_updater"
-	set/p version_remote=<%version_file_remote%
-	(echo version_remote=%version_remote%)>> "%tmp_infos_file%"
-)
 
 :: ---- RETROBAT PATHS ----
 
@@ -58,7 +27,7 @@ if not "%script_type%"=="builder" (
 if "%archx%"=="x86_64" (echo msys_path=%SystemDrive%\msys64)>> "%tmp_infos_file%"
 if "%archx%"=="x86" (echo msys_path=%SystemDrive%\msys)>> "%tmp_infos_file%"
 
-if not "%script_type%"=="builder" (
+if not "%script_type%" == "builder" (
 
 	(echo batgui_path=!root_path!)>> "%tmp_infos_file%"
 	(echo batocera_ports_path=!root_path!\emulationstation)>> "%tmp_infos_file%"
@@ -103,36 +72,60 @@ if not "%script_type%"=="builder" (
 	(echo wiimotegun_path=!build_path!\emulationstation)>> "%tmp_infos_file%"
 )
 	
-
 :: ---- URLS ----
 
-if not "%script_type%"=="builder" (
+set installroot_url=https://www.retrobat.ovh
 
-	(echo batgui_url=https://www.retrobat.ovh/repo/%arch%/%branch%)>> "%tmp_infos_file%"
-	(echo batocera_ports_url=https://github.com/kaylh/batocera-ports/releases/download/continuous)>> "%tmp_infos_file%"
-	(echo bios_url=https://github.com/kaylh/RetroBat-BIOS/archive/refs/heads)>> "%tmp_infos_file%"
-	(echo decorations_url=https://github.com/kaylh/batocera-bezel.git)>> "%tmp_infos_file%"
-	(echo default_theme_url=https://github.com/fabricecaruso/es-theme-carbon/archive/refs/heads)>> "%tmp_infos_file%"
-	(echo emulationstation_url=https://github.com/fabricecaruso/batocera-emulationstation/releases/download/continuous-master)>> "%tmp_infos_file%"
-	(echo lrcores_url=https://buildbot.libretro.com/nightly/windows/%archx%/latest)>> "%tmp_infos_file%"
-	(echo mega_bezels_url=https://www.retrobat.ovh/repo/medias)>> "%tmp_infos_file%"
-	(echo releases_url=https://www.retrobat.ovh/releases)>> "%tmp_infos_file%"
-	(echo retroarch_url=https://buildbot.libretro.com/stable/%retroarch_version%/windows/%archx%)>> "%tmp_infos_file%"
-	(echo retrobat_url=https://www.retrobat.ovh/repo/%arch%/%branch%)>> "%tmp_infos_file%"
-	(echo wiimotegun_url=https://github.com/fabricecaruso/WiimoteGun/releases/download/v1.0)>> "%tmp_infos_file%"
+if not "%script_type%" == "builder" (
+
+	(echo archive_url=%installroot_url%/repo/%arch%/%branch%/archives)>> "%tmp_infos_file%"
 
 ) else (
 
-	(echo batgui_url=https://www.retrobat.ovh/repo/%arch%/%branch%)>> "%tmp_infos_file%"
+	(echo batgui_url=%installroot_url%/repo/%arch%/%branch%)>> "%tmp_infos_file%"
 	(echo batocera_ports_url=https://github.com/kaylh/batocera-ports/releases/download/continuous)>> "%tmp_infos_file%"	
 	(echo emulationstation_url=https://github.com/fabricecaruso/batocera-emulationstation/releases/download/continuous-master)>> "%tmp_infos_file%"
-	(echo emulators_url=https://www.retrobat.ovh/repo/%arch%/%branch%/emulators)>> "%tmp_infos_file%"
+	(echo emulators_url=%installroot_url%/repo/%arch%/%branch%/emulators)>> "%tmp_infos_file%"
 	(echo lrcores_url=https://buildbot.libretro.com/nightly/windows/%archx%/latest)>> "%tmp_infos_file%"
-	(echo mega_bezels_url=https://www.retrobat.ovh/repo/medias)>> "%tmp_infos_file%"
+	(echo mega_bezels_url=%installroot_url%/repo/medias)>> "%tmp_infos_file%"
 	(echo retroarch_url=https://buildbot.libretro.com/stable/%retroarch_version%/windows/%archx%)>> "%tmp_infos_file%"
-	(echo retrobat_binaries_url=https://www.retrobat.ovh/repo/tools)>> "%tmp_infos_file%"
+	(echo retrobat_binaries_url=%installroot_url%/repo/tools)>> "%tmp_infos_file%"
 	(echo wiimotegun_url=https://github.com/fabricecaruso/WiimoteGun/releases/download/v1.0)>> "%tmp_infos_file%"
 
+)
+
+:: ---- SET VERSIONS ----
+
+set version_file_local=version.info
+set version_file_remote=remote_version.info
+
+if exist "%root_path%\emulationstation\%version_file_local%" (
+
+	set/P version_local=<"%root_path%\emulationstation\%version_file_local%"
+	echo version_local=!version_local!>> "%tmp_infos_file%"
+)
+
+if not "%script_type%" == "builder" (
+	
+	if exist "%root_path%\system\modules\rb_updater\%version_file_remote%" del/Q "%root_path%\system\modules\rb_updater\%version_file_remote%" >nul
+	"%root_path%\system\modules\rb_updater\wget" --no-check-certificate --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 3 -P "%root_path%\system\modules\rb_updater" %installroot_url%/repo/%arch%/%branch%/%version_local%/%version_file_remote% -q
+
+	if not exist "%root_path%\system\modules\rb_updater\%version_file_remote%" (
+	
+		set/A exit_code=2
+		goto :eof
+		
+	) else (
+	
+		set/A exit_code=0
+	)
+)
+
+if exist "%root_path%\system\modules\rb_updater\%version_file_remote%" (
+	
+	set/P version_remote=<"%root_path%\system\modules\rb_updater\%version_file_remote%"
+	(echo version_remote=!version_remote!)>> "%tmp_infos_file%"
+	
 )
 
 :: ---- GO BACK TO PARENT SCRIPT ----
